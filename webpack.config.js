76d3566fe89ab0ce2ebde46f5443bde5571822bc
juicyjusung <juicyjusung@gmail.com>
2020-03-16 03:25:48 +0900

    feat: google font 및 style 환경 추가

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,40 @@ module.exports = {
       '@': require('path').resolve(__dirname, 'src'),
     },
   },
+  module: {
+    rules: [
+      // SASS has different line endings than SCSS
+      // and cannot use semicolons in the markup
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              // This is the path to your variables
+              prependData: "@import '@/styles/variables.scss'",
+            },
+          },
+        ],
+      },
+      // SCSS has different line endings than SASS
+      // and needs a semicolon after the import.
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              // This is the path to your variables
+              prependData: "@import '@/styles/variables.scss';",
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
