60a6559c5e4fc71a8ffd6f4df01247ba98e6ee2d
juicyjusung <juicyjusung@gmail.com>
2020-03-18 01:40:44 +0900

    delete: Snackbar 미사용으로 패키지 및 컴포넌트 삭제

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 import RosPlugin from './plugins/ros';
 import UtilPlugin from './plugins/util';
-import VuetifySnackbarQueue from 'vuetify-snackbar-queue';
 import PerfectScrollbar from 'vue2-perfect-scrollbar';
 import 'vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css';
 import '@/assets/css/global.scss';
@@ -17,7 +16,6 @@ Vue.config.productionTip = false;
 
 Vue.use(UtilPlugin, { store });
 Vue.use(RosPlugin, { store });
-Vue.use(VuetifySnackbarQueue);
 Vue.use(PerfectScrollbar);
 Vue.use(Notifications);
 
